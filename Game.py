9a56db9f2b1cd4b103d7bf0a1f68a4e86e146ec1
Julian Floreau <julian.floreau@gmail.com>
2020-03-20 12:46:02 +0100

    Ajout de la docstring de Game.py

--- Game.py
+++ Game.py
@@ -17,6 +17,19 @@ class Game:
   
   #CONSTRUCTEUR  
   def __init__(self,H=9,L=100):
+    """
+    Default Contructor
+    
+    Parameters
+    ----------
+    self : class
+      Object Game creat here
+
+    H : int
+      The heigt of the grid's game
+    L : int
+      The length of the grid's game
+    """
     #Grid
     self.Grid=np.zeros((H,L),dtype=int)#The grid is a table full of zeros (H columns x L rows)
     self.Grid[H-2:H,:]=np.ones((2,L),dtype=int)#The grid has the 2 lines at the bottom full of ones (the floor)
@@ -33,18 +46,60 @@ class Game:
 
   #AFFICHAGE  
   def initdraw(self):
+    """
+    Generate a game area
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     self.World=ViewWorld.CreateWorld()
     self.World.pack(padx=000,pady=000)
     self.B = tk.Button(master=self.World.frame, text="Run Bobby, RUUUN !", bg='yellow', fg='red', width=25, height = 5, command=lambda:self.run()).pack(side=tk.LEFT)
+  
   def printgridstep(self):
+    """
+    Print the game area
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     self.initdraw()
     self.World.draw_grid(self.Grid)
     
   #TERRAIN  
-  def MakePit(self,x): #Fait un trou à la position x
+  def MakePit(self,x): 
+    """
+    Creat a hole a the x position
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    x : int
+      The position where the hole is creat
+    """
     self.Grid[2:,x]=np.zeros((len(self.Grid[2:,x])),dtype=int)
 
   def AddBlockStratum(self,xl,xr): #Ajoute une strate de bloques entre xl et xr
+    """
+    Add a layer of blocks between the position x1 and xr
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    xl : int
+      The start position of the layer
+
+    xr : int
+      The end position of the layer
+    """
     H=len(self.Grid)
     for col in range(xl,xr+1):
       i=4
@@ -54,6 +109,20 @@ class Game:
         self.Grid[i-1][col]=1
         
   def Random_Level_generation(self,nbPit,nblayer):
+    """
+    Generate a random level with a nbPit hole and a nblayer layer
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    nbPit : int
+      The number of hole on the board
+
+    nblayer : int
+      The number of layer on the board
+    """
     for i in range(nbPit):
       x=int(np.random.random()*(len(self.Grid[0,:])-5)+2)
       if self.Grid[len(self.Grid)-1,x-1]!=0 or self.Grid[len(self.Grid)-1,x+1]!=0:
@@ -72,7 +141,18 @@ class Game:
     return True
   
   
-  def File_to_map(self,s):#Charge un terrain depuis un fichier texte
+  def File_to_map(self,s):
+    """
+    Load a map from a text file (.txt)
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    s : str
+      the file name where the map is located
+    """  
     self.Grid=np.loadtxt(s)
     self.lenth=len(self.Grid[0,:])
     self.hight=len(self.Grid[:,0])
@@ -80,11 +160,30 @@ class Game:
       agent.Environment_=self.Grid
       
   #POPULATION
-  def AddAgent(self,agent):#ajout d'un agent dans la population
+  def AddAgent(self,agent):
+    """
+    Add an agent at the population
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    agent : class
+      Object Agent, an individual who will try the map
+    """
     self.Pop.append(agent)  
     agent.Environment_=self.Grid
     
-  def SortByFitness(self): #tri la popuolation des individus avec la meilleur fitnesse à ceux avec la pire
+  def SortByFitness(self):
+    """
+    Sort descending the population of individual depending the fitness, from the best to the worst
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     for agent in self.Pop:
       if not agent.Alive:
         agent.posX_=-100 #
@@ -96,7 +195,29 @@ class Game:
           self.Pop[j]=self.Pop[j+1]
           self.Pop[j+1]=inter   
       i=i-1
-  def New_Generation(self,Methode=0,Indiv=50,Mute=10):#crée une nouvelle génération d'individus à partir d'une ancienne population triée par fitness en fonction de la methode choisie
+
+  def New_Generation(self,Methode=0,Indiv=50,Mute=10):
+    """
+    Create a new generation of individual from the descending sort population.
+    Two methode are possible :
+      - Methode 0 : only the best individual can have children, it is him genome who is mutate
+      - Methode 1 : the 50% best individuals have children by mutation of them genome
+    By default the choosen methode is the methode 0
+
+    Parameters
+    ----------
+    self : class
+      Object Game
+
+    Methode : int
+      The methode choose for the generation of indivual, can take the values 0 or 1, by default methode select is 0
+
+    Indiv : int
+      Number of individual at the new generation, by default 50
+
+    Mute : int
+      Number of mutation by genome desire, by default 10
+    """    
     if Methode==0:
       Father=self.Pop[0]
       self.Pop=[]
@@ -126,6 +247,14 @@ class Game:
       self.Pop=PopBis
     
   def Start(self):
+    """
+    Set up the begin of the game by positionning the individuals on the game area
+
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     self.Time=0
     for agent in self.Pop:
       agent.Alive=True
@@ -135,8 +264,16 @@ class Game:
         agent.Genome_.Hidden_=np.zeros((1,agent.Genome_.H_))
 
 #JEU
-  def run(self):#fait bouger l'ensemble des individus une fois et les affiches
-      self.Time+=1
+  def run(self):
+    """
+    Run the game, move all the individual once and print them
+
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """      
+    self.Time+=1
       for Ag in self.Pop:
         if Ag.Alive:
           self.Grid[Ag.posY_,Ag.posX_]=0
@@ -154,6 +291,14 @@ class Game:
           self.Grid[Ag.posY_,Ag.posX_]=0
         
   def RunBlind(self): #run sans affichage
+    """
+    Run the game, move all the individual once whitout print them
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     self.Time+=1
     for Ag in self.Pop:
       if Ag.Alive:
@@ -164,6 +309,15 @@ class Game:
         Ag.MvBackward()
         
   def PopTest(self):
+    """
+    Check if the time is inferior to 2*length of the game area
+    Check if some individual just make round trip without progress in the game area
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """
     while self.Time<2*self.lenth:
       self.RunBlind()
     self.Time=0
@@ -176,6 +330,14 @@ class Game:
 
 #EVOLUTION
   def FindBestAgent(self):
+    """
+    Find the best agent, the individual who arrived first
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    """    
     best=0
     x=0
     for agent in range(len(self.Pop)):#finds a better one if it exists
@@ -186,6 +348,26 @@ class Game:
   
 
   def EvolveByDivision(self,IndivMax,MutationsRate,Generation=500):
+    """
+    Calculate the time to solve the game
+    Each individual can have children (if population is not full) but the individual with the best fitness begin
+    
+    Parameters
+    ----------
+    self : class
+      Object Game
+    IndivMax : int
+      Maximal number of individual in the population
+    MutatopnsRate : int
+      Number of mutation
+    Generation : int
+      Number of generation create, by default equal to 500
+
+    Returns
+    -------
+    int
+      time made by a individual in the population after several mutation to complite the game
+    """
     t=time.time()
     end=False;
     for i in range(Generation):
@@ -216,6 +398,26 @@ class Game:
     return time.time()-t
 
   def Evolve(self,Children,MutationsRate,Generation=500):
+    """
+    Calculate the time to solve the game
+    Only the individual with the best fitness have child
+
+    Parameters
+    ----------
+    self : class
+      Object Game
+    Children : int
+      Number of children of the best individual
+    MutatopnsRate : int
+      Number of mutation
+    Generation : int
+      Number of generation create, by default equal to 500
+
+    Returns
+    -------
+    int
+      time made by a individual in the population after several mutation to complite the game
+    """
     t=time.time()
     end=False
     for i in range(Generation):
@@ -236,6 +438,32 @@ class Game:
     return time.time()-t
 
   def Evolution(self,Methode=0,Indiv=50,Mute=10,timeMax=10):#méthode d'évolution généralisée
+    """
+    Calculate the time to solve the game
+    Two methode of children are possible :
+      - Methode 0 : only the best individual can have children, it is him genome who is mutate
+      - Methode 1 : the 50% best individuals have children by mutation of them genome
+
+    Parameters
+    ----------
+    self : class
+      Object Game
+    Methode : int
+      The methode choose for the generation of indivual, can take the values 0 or 1, by default methode select is 0
+    Indiv : int
+      The number of individual in the new population, by default equal to 50
+    Mute : int
+      Number of mutation, by default equal to 10
+    timeMax : int
+      Maximal time, if the game is not solve stop the function, by default equal to 10
+
+    Returns
+    -------
+    int
+      time made by a individual in the population after several mutation to complite the game
+    int
+      number of generation create to solve the game
+    """  
     t=time.time()
     Finished=False
     generation=0
