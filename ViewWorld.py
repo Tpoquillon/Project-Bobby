29ca2bb45eea9ba8b9238d93da670f2073c464df
bscassady <baptiste.sorin@insa-lyon.fr>
2020-03-26 10:01:45 +0100

    Ce truc marche pas

--- ViewWorld.py
+++ ViewWorld.py
@@ -3,7 +3,7 @@ import time
 class CreateWorld(tk.Canvas):
   def __init__(self):
 
-	"""
+    """
     Creates a window
 
   
@@ -13,7 +13,7 @@ class CreateWorld(tk.Canvas):
     Parameters
     ----------
     self : class
-    Object CreateWorld in the library tkinder
+    Object CreateWorld in the library tkinter
     """
     self.window = tk.Tk() #Creating a window
     self.window.title("Fantastic Bobby")
@@ -26,7 +26,7 @@ class CreateWorld(tk.Canvas):
 				
   def on_resize(self,event):
 
-	"""
+	  """
     Determines the ratio of old width/height to new width/height, resizes the canvas, rescales all the objects tagged with the "all" tag
 
 
@@ -39,18 +39,18 @@ class CreateWorld(tk.Canvas):
       Object CreateWorld in the library tkinder
     """
 	# determine the ratio of old width/height to new width/height
-    	wscale = float(event.width)/self.width
-    	hscale = float(event.height)/self.height
-    	self.width = event.width
-    	self.height = event.height
-    	# resize the canvas 
-    	self.config(width=self.width, height=self.height)
-    	# rescale all the objects tagged with the "all" tag
-    	self.scale("all",0,0,wscale,hscale)
+    wscale = float(event.width)/self.width
+    hscale = float(event.height)/self.height
+    self.width = event.width
+    self.height = event.height
+    # resize the canvas 
+    self.config(width=self.width, height=self.height)
+    # rescale all the objects tagged with the "all" tag
+    self.scale("all",0,0,wscale,hscale)
 	
   def draw_grid(self, Grid):
 
-	"""
+	  """
     Draws a grid and the game area
 
 
@@ -63,33 +63,33 @@ class CreateWorld(tk.Canvas):
       A list of list of int contains the game area
     """
 
-	self.w = len(Grid[0])
-	self.h = len(Grid)
-	self.pixels_height = int(self.height)/self.h
-	self.pixels_width = int(self.width)/self.w
-	for i in range(self.h):
-	    for j in range(self.w):
-	        if (Grid[i][j]==0):
-	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
-	        elif (Grid[i][j]==2):
-	            #imglabel = tk.Label(image=self.img)
-	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
-	            self.create_oval(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="cyan")
-	            self.create_oval((0.5+j)*self.pixels_width,(i+0.2)*self.pixels_height, (j+1-0.2)*self.pixels_width,(i+1-0.4)*self.pixels_height,fill="white")
-	            self.create_oval((0.65+j)*self.pixels_width,(i+0.3)*self.pixels_height, (j+1-0.25)*self.pixels_width,(i+1-0.6)*self.pixels_height,fill="black")
+    self.w = len(Grid[0])
+    self.h = len(Grid)
+    self.pixels_height = int(self.height)/self.h
+    self.pixels_width = int(self.width)/self.w
+    for i in range(self.h):
+        for j in range(self.w):
+            if (Grid[i][j]==0):
+                self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
+            elif (Grid[i][j]==2):
+                #imglabel = tk.Label(image=self.img)
+                self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
+                self.create_oval(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="cyan")
+                self.create_oval((0.5+j)*self.pixels_width,(i+0.2)*self.pixels_height, (j+1-0.2)*self.pixels_width,(i+1-0.4)*self.pixels_height,fill="white")
+                self.create_oval((0.65+j)*self.pixels_width,(i+0.3)*self.pixels_height, (j+1-0.25)*self.pixels_width,(i+1-0.6)*self.pixels_height,fill="black")
 
-    	self.w = len(Grid[0])
-    	self.h = len(Grid)
-    	self.pixels_height = int(self.height)/self.h
-    	self.pixels_width = int(self.width)/self.w
-    	for i in range(self.h):
-    	    for j in range(self.w):
-    	        if (Grid[i][j]==0):
-    	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
-    	        elif (Grid[i][j]==2):
-    	            #imglabel = tk.Label(image=self.img)
-    	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
-    	            self.create_oval(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="cyan")
-    	            self.create_oval((0.5+j)*self.pixels_width,(i+0.2)*self.pixels_height, (j+1-0.2)*self.pixels_width,(i+1-0.4)*self.pixels_height,fill="white")
-    	            self.create_oval((0.65+j)*self.pixels_width,(i+0.3)*self.pixels_height, (j+1-0.25)*self.pixels_width,(i+1-0.6)*self.pixels_height,fill="black")
+      	self.w = len(Grid[0])
+      	self.h = len(Grid)
+      	self.pixels_height = int(self.height)/self.h
+      	self.pixels_width = int(self.width)/self.w
+      	for i in range(self.h):
+      	    for j in range(self.w):
+      	        if (Grid[i][j]==0):
+      	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
+      	        elif (Grid[i][j]==2):
+      	            #imglabel = tk.Label(image=self.img)
+      	            self.create_rectangle(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="blue", width=0)
+      	            self.create_oval(j*self.pixels_width,i*self.pixels_height, (j+1)*self.pixels_width,(i+1)*self.pixels_height,fill="cyan")
+      	            self.create_oval((0.5+j)*self.pixels_width,(i+0.2)*self.pixels_height, (j+1-0.2)*self.pixels_width,(i+1-0.4)*self.pixels_height,fill="white")
+      	            self.create_oval((0.65+j)*self.pixels_width,(i+0.3)*self.pixels_height, (j+1-0.25)*self.pixels_width,(i+1-0.6)*self.pixels_height,fill="black")
 
